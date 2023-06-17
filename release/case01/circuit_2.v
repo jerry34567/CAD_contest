57068184ae9520158a18aae476c18f326d33d29f
嚴培勻 <10040905paron@gmail.com>
2023-06-17 13:37:30 +0800

    combined gv with sat

--- release/case01/circuit_2.v
+++ release/case01/circuit_2.v
@@ -2,11 +2,11 @@ module top ( x0 , x1 , y0 , y1 , z , u0 , u1 , w0 , w1 ) ;
     input x0 , x1 , y0 , y1 , z ;
     output u0 , u1 , w0 , w1 ;
     wire n1 , n2 , n3 ;
-    nand ( n1 , x1 , y0 ) ;
-    nand ( n2 , x0 , y1 ) ;
-    nand ( n3 , y0 , z ) ;
-    and ( u0 , y0 , x0 ) ;
-    xor ( u1 , n1 , n2 ) ;
-    xnor ( w0 , y1 , n3 ) ;
-    xor ( w1 , y0 , z ) ;
+    nand g1 ( n1 , x1 , y0 ) ;
+    nand g2 ( n2 , x0 , y1 ) ;
+    nand g3 ( n3 , y0 , z ) ;
+    and g4 ( u0 , y0 , x0 ) ;
+    xor g5 ( u1 , n1 , n2 ) ;
+    xnor g6 ( w0 , y1 , n3 ) ;
+    xor g7 ( w1 , y0 , z ) ;
 endmodule
